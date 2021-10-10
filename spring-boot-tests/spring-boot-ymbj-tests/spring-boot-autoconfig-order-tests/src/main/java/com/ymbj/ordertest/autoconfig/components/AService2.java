29a7a8fbd248ba5d3ff39dcfe37decbb2f333a8f
yuanmabiji <13570990660@163.com>
2021-10-10 23:29:43 +0800

    项目内若优先需要创建插件类的某个bean，可以利用项目内的某个bean @DependsOn 插件内的某个bean，这样插件内的某个bean就会优先创建啦

--- spring-boot-tests/spring-boot-ymbj-tests/spring-boot-autoconfig-order-tests/src/main/java/com/ymbj/ordertest/autoconfig/components/AService2.java
+++ spring-boot-tests/spring-boot-ymbj-tests/spring-boot-autoconfig-order-tests/src/main/java/com/ymbj/ordertest/autoconfig/components/AService2.java
@@ -1,9 +1,17 @@
 package com.ymbj.ordertest.autoconfig.components;
 
+import com.ymbj.ordertest.autoconfig.annoorderbean.AutoConfigurationAnnoBean11;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
 
+// 【结论】项目内若优先需要创建插件类的某个bean，可以利用项目内的某个bean @DependsOn 插件内的某个bean，这样插件内的某个bean就会优先创建啦
+@DependsOn("autoConfigurationAnnoBean11")
 @Service
 public class AService2 {
+	/*@Autowired
+	private AutoConfigurationAnnoBean11 autoConfigurationAnnoBean11;*/
+
 	public AService2() {
 		System.out.println("=========AService2============");
 	}
